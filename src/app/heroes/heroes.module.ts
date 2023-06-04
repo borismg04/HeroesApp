3cddad5e62a473cea497c0dbc8903ff88190c12d
bmonroyg <bmonroyg@emeal.nttdata.com>
2023-06-03 23:36:30 -0500

    initial crud

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
 import { HeroImagePipe } from './pipes/hero-image.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -26,8 +27,10 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     HeroesRoutingModule,
-    MaterialModule
+    MaterialModule,
+
   ]
 })
 export class HeroesModule { }
